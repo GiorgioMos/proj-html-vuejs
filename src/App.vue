e59bf51426444420f92a091bc84227e095b9f269
GiorgioMos <giorgiomosca2@gmail.com>
2023-12-11 18:19:31 +0100

    Nav Bar almost done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup>
-import AppHeader from './components/AppHeader.vue'
-import AppMain from './components/AppMain.vue'
-import AppFooter from './components/AppFooter.vue'
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter,
+    },
+    data() {
+        return {
+
+        }
+    }
+}
 </script>
 
 <template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,63 @@
 <script>
-
+export default {
+    data() {
+        return {
+            NavItems: [
+                { name: 'Home' },
+                { name: 'Pages' },
+                { name: 'Courses' },
+                { name: 'Features' },
+                { name: 'Blog' },
+                { name: 'Shop' }
+            ]
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <div id="nav">
+        <a href="#"><img src="../assets/img/dark-logo.png" alt="Logo"></a>
+        <ul>
+            <li v-for="NavItem in NavItems">{{ NavItem.name }} <i class="fa-solid fa-angle-down fa-2xs"></i></li>
+        </ul>
+        <a class="user-icon" href="#"><i class="fa-regular fa-circle-user fa-lg"></i></a>
+        <input type="text" placeholder="Search..." name="search">
+        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#nav {
+    display: flex;
+    height: 70px;
+    box-shadow: 0px 10px 30px #F5F1ED;
+    justify-content: center;
+    align-items: center;
+    padding: 0 7%;
+
+    img {
+        width: 150px;
+        margin-right: 150px;
+    }
+
+    input {
+        margin-left: 20px;
+    }
+
+    .user-icon {
+        color: #20AD96;
+    }
+
+}
+
+ul {
+    list-style-type: none;
+    display: flex;
+    padding: 0;
+
+    li {
+        margin: 1rem;
+    }
+}
+</style>
